022b6d735b615c37fdc40db193ede3b31fb1e8fe
Christian Solje <csolje@gmail.com>
2023-07-29 10:32:16 +0200

    chore(router): Changed what pane is attached

--- bin/mount_nas.sh
+++ bin/mount_nas.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sudo mount -t cifs //10.0.0.4/Backup /home/chrs/nas/Backup -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
-sudo mount -t cifs //10.0.0.4/Pictures /home/chrs/nas/Pictures -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
-sudo mount -t cifs //10.0.0.4/Movies /home/chrs/nas/Movies -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
-sudo mount -t cifs //10.0.0.4/Music /home/chrs/nas/Music -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
-#sudo mount -t smb3 //10.0.0.4/Movies /home/chrs/nas/Movies -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
-#sudo mount -t smb3 //10.0.0.4/Backup /home/chrs/nas/Backup -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
-#sudo mount -t smb3 //10.0.0.4/Music /home/chrs/nas/Music -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
-#sudo mount -t smb3 //10.0.0.4/Pictures /home/chrs/nas/Pictures -o username=cso,password='user password',workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t cifs //10.1.1.4/Backup /home/chrs/nas/Backup -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t cifs //10.1.1.4/Pictures /home/chrs/nas/Pictures -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t cifs //10.1.1.4/Movies /home/chrs/nas/Movies -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t cifs //10.1.1.4/Music /home/chrs/nas/Music -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t smb2 //10.1.1.4/Movies /home/chrs/nas/Movies -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t smb2 //10.1.1.4/Backup /home/chrs/nas/Backup -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t smb2 //10.1.1.4/Music /home/chrs/nas/Music -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t smb2 //10.1.1.4/Pictures /home/chrs/nas/Pictures -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
