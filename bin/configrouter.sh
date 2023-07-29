530f83b41a7debe64a7adfc53d4b243c242a341e
Christian Solje <csolje@gmail.com>
2023-07-29 10:13:27 +0200

    chore(router): Changed what pane is attached

--- bin/configrouter.sh
+++ bin/configrouter.sh
@@ -6,4 +6,4 @@ if ! tmux has-session -t router; then
     tmux new-window -ngarage -t router:3
 #    tmux send-keys -t router:1 vim Enter
 fi
-tmux attach -t router
+tmux attach -t router \; select-window -t owned \;
