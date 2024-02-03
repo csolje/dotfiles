25bea6bc67ae399e0c53990660ba0f0ad90050f6
Christian Solje <csolje@gmail.com>
2024-02-03 16:22:17 +0100

    chore(dotfiles): fix username and password

--- bin/mount_nas.sh
+++ bin/mount_nas.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#sudo mount -t cifs //10.1.1.4/Backup /home/chrs/nas/Backup -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
-#sudo mount -t cifs //10.1.1.4/Pictures /home/chrs/nas/Pictures -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
-#sudo mount -t cifs //10.1.1.4/Movies /home/chrs/nas/Movies -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
-#sudo mount -t cifs //10.1.1.4/Music /home/chrs/nas/Music -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
-sudo mount -t smb2 //10.1.1.4/Movies /home/chrs/nas/Movies -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
-sudo mount -t smb2 //10.1.1.4/Backup /home/chrs/nas/Backup -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
-sudo mount -t smb2 //10.1.1.4/Music /home/chrs/nas/Music -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
-sudo mount -t smb2 //10.1.1.4/Pictures /home/chrs/nas/Pictures -o username=<username>,password=<password>,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t cifs //10.1.1.4/Backup /home/chrs/nas/Backup -o username=xxxx,password=xxxxx,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t cifs //10.1.1.4/Pictures /home/chrs/nas/Pictures -o username=xxxx,password=xxxxxx,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t cifs //10.1.1.4/Movies /home/chrs/nas/Movies -o username=xxxx,password=xxxxxx,workgroup=workgroup,uid=chrs,gid=chrs
+sudo mount -t cifs //10.1.1.4/Music /home/chrs/nas/Music -o username=xxxx,password=xxxxxx,workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t smb2 //10.1.1.4/Movies /home/chrs/nas/Movies -o username=xxxx,password=xxxxxx,workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t smb2 //10.1.1.4/Backup /home/chrs/nas/Backup -o username=xxxx,password=xxxxxx,workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t smb2 //10.1.1.4/Music /home/chrs/nas/Music -o username=xxxx,password=xxxxxx,workgroup=workgroup,uid=chrs,gid=chrs
+#sudo mount -t smb2 //10.1.1.4/Pictures /home/chrs/nas/Pictures -o username=xxxx,password=xxxxxx,workgroup=workgroup,uid=chrs,gid=chrs
