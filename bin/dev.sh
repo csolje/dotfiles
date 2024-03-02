7c428f50c26dbbb6617579640229f8e57feb73e4
Christian Solje <csolje@gmail.com>
2024-03-02 15:03:46 +0100

    feat: Deleted old script files

--- bin/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if ! tmux has-session -t code; then
-    tmux new -s code -ncode -d
-    tmux new-window -ndiff -t code:2
-    tmux new-window -nbuild -t code:3
-    tmux split-window -t code:diff -h
-    tmux send-keys -t code:1 vim Enter
-fi
-tmux attach -t code
